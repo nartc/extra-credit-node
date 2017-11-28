5952d3543139533a97e30fbd1efa8ef42669b026
Chau <ctch5@mail.umsl.edu>
2017-11-28 16:35:18 -0600

    still fixing stuffs

--- routes/professors.js
+++ routes/professors.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const cors = require('cors');
 
 const config = require("../config/keys");
 const emailHelper = require("../helper/email-helper");
@@ -17,7 +16,7 @@ const jwtOptions = {
 };
 
 //Init Professor Account
-router.post('/init', cors(), (req, res) => {
+router.post('/init', (req, res) => {
   console.log(req.body);
   const emailInput = req.body.email;
   const emailQuery = {
@@ -26,6 +25,7 @@ router.post('/init', cors(), (req, res) => {
 
   //Check if there's already a professor with this email in the db
   Professor.getProfByEmail(emailInput, (err, professor) => {
+    console.log('iaminhere');
     if (err) {
       return res.status(500).json({
         success: false,
